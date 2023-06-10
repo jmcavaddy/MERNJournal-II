c142260fa948013f74d81c03a2626c6561df8fd1
jmcavaddy <jakemcavaddy@gmail.com>
2023-06-10 10:02:32 -0400

    Added entry info to cards displayed on Notes page

--- client/src/components/pages/CreateNote.js
+++ client/src/components/pages/CreateNote.js
@@ -45,43 +45,41 @@ const CreateNote = () => {
   };
 
   return (
-    <div className="card bg-white card-rounded w-25">
-      <div className="card-header bg-dark text-center">
-        <h1>Let's create a matchup!</h1>
-      </div>
-      <div className="card-body m-5">
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <form onSubmit={handleFormSubmit}>
-            <label>Tech 1: </label>
-            <select name="tech1" onChange={handleInputChange}>
-              {techList.map((tech) => {
-                return (
-                  <option key={tech._id} value={tech.name}>
-                    {tech.name}
-                  </option>
-                );
-              })}
-            </select>
-            <label>Tech 2: </label>
-            <select name="tech2" onChange={handleInputChange}>
-              {techList.map((tech) => {
-                return (
-                  <option key={tech._id} value={tech.name}>
-                    {tech.name}
-                  </option>
-                );
-              })}
-            </select>
-            <button className="btn btn-danger" type="submit">
-              Create Matchup!
-            </button>
-          </form>
-        )}
-      </div>
-      {error && <div>Something went wrong...</div>}
+    <form
+    style={{
+      border: "2px solid blue",
+      borderRadius: "10px",
+      padding: "30px",
+    }}
+  >
+    <div className="mb-3">
+      <label htmlFor="Title" className="form-label">
+        Note's title
+      </label>
+      <input
+        type="text"
+        className="form-control"
+        id="title"
+        aria-describedby="titleHelp"
+        placeholder="Note Title"
+      />
     </div>
+    <div className="mb-3">
+      <label for="Note" className="form-label">
+        Note
+      </label>
+      <textarea
+        type="text"
+        className="form-control"
+        id="Note"
+        placeholder="Enter your Note"
+      ></textarea>
+    </div>
+
+    <button type="submit" className="btn btn-primary">
+      Submit
+    </button>
+  </form>
   );
 };
 
--- client/src/components/pages/index.js
+++ client/src/components/pages/index.js
@@ -5,83 +5,64 @@ import { QUERY_SINGLE_PROFILE } from "../../utils/queries";
 import "../../App";
 import Navbar from "../Navbar";
 import { ADD_ENTRY } from "../../utils/mutations";
+import { QUERY_ME } from '../../utils/queries';
+import CreateNote from "./CreateNote";
+
 
 const Notes = () => {
-  const [notes, setNotes] = useState([]);
+  const { loading, data } = useQuery(QUERY_ME);
 
-  const { loading, data } = useQuery(QUERY_SINGLE_PROFILE, {
-    variables: { profileUsername: 'hellohellotesting' },
-  });
+  const [userData, setUserData] = useState({});
+
+  // use this to determine if `useEffect()` hook needs to run again
+  const userDataLength = Object.keys(userData).length;
 
   useEffect(() => {
-    if (!loading && data) {
-      setNotes(data.profile.entries);
-    }
-  }, [loading, data]);
+      if (data) {
+        setUserData(data.me);
+      }
+  }, [data]);
 
-  console.log("notes", notes);
+  // if data isn't here yet, say so
+  if (!userDataLength) {
+    return <h2>LOADING...</h2>;
+  }
 
-;
+  if (!userData?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this. Use the navigation links above to
+        sign up or log in!
+      </h4>
+    );
+  }
 
-  
   return (
     <div className="container">
       <div className="row justify-content">
         <div className="col-md-10">
           <div>
             <h1 className="my-4 text-center">My Notes</h1>
-            <form
-              style={{
-                border: "2px solid blue",
-                borderRadius: "10px",
-                padding: "30px",
-              }}
-            >
-              <div className="mb-3">
-                <label htmlFor="Title" className="form-label">
-                  Note's title
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="title"
-                  aria-describedby="titleHelp"
-                  placeholder="Note Title"
-                />
-              </div>
-              <div className="mb-3">
-                <label for="Note" className="form-label">
-                  Note
-                </label>
-                <textarea
-                  type="text"
-                  className="form-control"
-                  id="Note"
-                  placeholder="Enter your Note"
-                ></textarea>
-              </div>
-
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </form>
-
+            <CreateNote />
             <div>
               <div className="my-4 mynotes row">
-                <Card className="card mx-2" style={{ width: "18rem" }}>
-                  <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">
-                      Card subtitle
-                    </Card.Subtitle>
-                    <Card.Text>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </Card.Text>
-                    <Card.Link href="#">Edit</Card.Link>
-                    <Card.Link href="#">Delete</Card.Link>
-                  </Card.Body>
-                </Card>
+                {userData.entries.map((entry) => {
+                  return (
+                  <Card key={entry._id} className="card mx-2" style={{ width: "18rem" }}>
+                    <Card.Body>
+                      <Card.Title>{entry.entryTitle}</Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted">
+                        {entry.createdAt}
+                      </Card.Subtitle>
+                      <Card.Text>
+                        {entry.entryContent}
+                      </Card.Text>
+                      <Card.Link href="#">Edit</Card.Link>
+                      <Card.Link href="#">Delete</Card.Link>
+                    </Card.Body>
+                  </Card>
+                  );
+                })};
               </div>
             </div>
           </div>
