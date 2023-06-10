c9391c9082991b29d8c19940e3eb191ccb4b6390
Ruchi <contactruchiagarwal@gmail.com>
2023-06-10 16:22:55 -0400

    added backg

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';	
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
 import LoginForm from '../LoginForm/index';
 import SignupForm from '../SignupForm/index';
